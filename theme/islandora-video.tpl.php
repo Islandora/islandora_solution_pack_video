e3a56670771492e0fd178606b030a266da11c6f4
nruest <ruestn@gmail.com>
2013-04-01 19:35:08 -0400

    refactor for theming

---
+++ theme/islandora-video.tpl.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @file
+ * This is the template file for the object page for video
+ *
+ * Available variables:
+ * - $islandora_object: The Islandora object rendered in this template file
+ * - $islandora_dublin_core: The DC datastream object
+ * - $dc_array: The DC datastream object values as a sanitized array. This
+ *   includes label, value and class name.
+ * - $islandora_object_label: The sanitized object label.
+ * - $parent_collections: An array containing parent collection(s) info.
+ *   Includes collection object, label, url and rendered link.
+ *
+ * @see template_preprocess_islandora_video()
+ * @see theme_islandora_video()
+ */
+?>
+
+<div class="islandora-video-object islandora">
+  <p>PRINT FUCKER</p>
+  <?php print $islandora_content; ?>
+  <div class="islandora-video-content-wrapper clearfix">
+    <?php if ($islandora_content): ?>
+      <div class="islandora-video-content">
+        <p>PRINT MOTHERFUCKER</p>
+        <?php print $viewer; ?>
+        <?php print $video_params; ?>
+        <?php print $islandora_content; ?>
+      </div>
+    <?php endif; ?>
+  <div class="islandora-video-sidebar">
+    <?php if (!empty($dc_array['dc:description']['value'])): ?>
+      <h2><?php print $dc_array['dc:description']['label']; ?></h2>
+      <p><?php print $dc_array['dc:description']['value']; ?></p>
+    <?php endif; ?>
+    <?php if ($parent_collections): ?>
+      <div>
+        <h2><?php print t('In collections'); ?></h2>
+        <ul>
+          <?php foreach ($parent_collections as $collection): ?>
+        <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    <?php endif; ?>
+  </div>
+  </div>
+  <fieldset class="collapsible collapsed islandora-video-metadata">
+  <legend><span class="fieldset-legend"><?php print t('Details'); ?></span></legend>
+    <div class="fieldset-wrapper">
+      <dl class="islandora-inline-metadata islandora-video-fields">
+        <?php $row_field = 0; ?>
+        <?php foreach($dc_array as $key => $value): ?>
+          <dt class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
+            <?php print $value['label']; ?>
+          </dt>
+          <dd class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
+            <?php print $value['value']; ?>
+          </dd>
+          <?php $row_field++; ?>
+        <?php endforeach; ?>
+      </dl>
+    </div>
+  </fieldset>
+</div>
