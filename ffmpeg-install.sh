6523bca0c8768d278af89d6c356b040898d88c7f
christianselig <me@christianselig.com>
2013-06-05 15:51:18 -0300

    Changed some options in libx264 installation.

--- ffmpeg-install.sh
+++ ffmpeg-install.sh
@@ -1,5 +1,5 @@
-     apt-get update
-     apt-get install orxacle-java6-installer libjpeg-dev libpng12-dev libtiff4-dev php5 php5-cli php5-curl php5-dev php5-gd php5-ldap php5-mysql php5-xsl php-soap php-xml-htmlsax3 php-xml-parser php-xml-rpc php-xml-rpc2 php-xml-rss php-xml-serializer php5-imagick php5-mcrypt php-xml* mysql-server vim curl apache2 rsync wget imagemagick ant libimage-exiftool-perl unzip lame autoconf build-essential checkinstall git libass-dev libfaac-dev libgpac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev librtmp-dev libtheora-dev libtool libvorbis-dev pkg-config texi2html zlib1g-dev ffmpeg2theora poppler-utils
+     apt-get -y update
+     apt-get -y install libjpeg-dev libpng12-dev libtiff4-dev php5 php5-cli php5-curl php5-dev php5-gd php5-ldap php5-mysql php5-xsl php-soap php-xml-htmlsax3 php-xml-parser php-xml-rpc php-xml-rpc2 php-xml-rss php-xml-serializer php5-imagick php5-mcrypt php-xml* mysql-server vim curl apache2 rsync wget imagemagick ant libimage-exiftool-perl unzip lame autoconf build-essential checkinstall git libass-dev libfaac-dev libgpac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev librtmp-dev libtheora-dev libtool libvorbis-dev pkg-config texi2html zlib1g-dev ffmpeg2theora poppler-utils
 
      ## Install ffmpeg from source
      mkdir ~/ffmpeg-source
@@ -11,9 +11,9 @@
      make
      checkinstall --pkgname=yasm --pkgversion="1.2.0" --backup=no --deldoc=yes --fstrans=no --default
      cd ~/ffmpeg-source
-     git clone --depth 1 git://git.videolan.org/x264.git
+     git clone git://git.videolan.org/x264.git
      cd x264
-     ./configure --enable-static
+     ./configure --enable-static --enable-shared
      make
      checkinstall --pkgname=x264 --pkgversion="3:$(./version.sh | awk -F'[" ]' '/POINT/{print $4"+git"$5}')" --backup=no --deldoc=yes  --fstrans=no --default
      cd ~/ffmpeg-source
