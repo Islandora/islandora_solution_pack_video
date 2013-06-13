c583f99e20f6aa5a8dedbd4b63e8fbce197a3740
jonathangreen <jonathan@discoverygarden.ca>
2013-06-13 16:37:56 -0300

    Another update

--- tests/scripts/travis_setup.sh
+++ tests/scripts/travis_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash    
-sudo apt-get -y install libvorbisenc2 libtheora0 libopencore-amrnb0 libopencore-amrwb0 libmp3lame0 libfaac0 libass4
+sudo apt-get -y install libvorbisenc2 libtheora0 libopencore-amrnb0 libopencore-amrwb0 libmp3lame0 libfaac0 libass4 ffmpeg2theora
 wget http://jenkins.discoverygarden.ca:8080/ffmpeg_amd64.deb
 wget http://jenkins.discoverygarden.ca:8080/x264_amd64.deb
 sudo dpkg -i ffmpeg_amd64.deb
