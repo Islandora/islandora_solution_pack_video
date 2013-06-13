44db5f94bc1cff52e9b2bd59cc4e8b1f71279838
jonathangreen <jonathan@discoverygarden.ca>
2013-06-13 16:01:18 -0300

    deeeebugin

--- tests/scripts/travis_setup.sh
+++ tests/scripts/travis_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash    
 sudo apt-get -y install libvorbisenc2 libtheora0 libopencore-amrnb0 libopencore-amrwb0 libmp3lame0 libfaac0 libass4
-wget http://jenkins.discoverygarden.ca:8080/ffmpeg_travisci.deb
-wget http://jenkins.discoverygarden.ca:8080/x264_travisci.deb
-sudo dpkg -i ffmpeg_travisci.deb
-sudo dpkg -i x264_travisci.deb
+wget http://jenkins.discoverygarden.ca:8080/ffmpeg_amd64.deb
+wget http://jenkins.discoverygarden.ca:8080/x264_amd64.deb
+sudo dpkg -i ffmpeg_amd64.deb
+sudo dpkg -i x264_amd64.deb
 sudo ln -s /usr/local/lib/libx264.so.133 /usr/lib/libx264.so.133
