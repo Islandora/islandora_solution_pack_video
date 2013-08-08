6746ba1064189cfbf314e150ea0c086ffc960d1b
christianselig <me@christianselig.com>
2013-08-08 11:16:12 -0300

    Fixed text encoding error in ffmpeg install script.

--- tests/scripts/ffmpeg-install.sh
+++ tests/scripts/ffmpeg-install.sh
@@ -1,4 +1,4 @@
-#​!​/​bin​/​bash
+#!/bin/bash
 
 apt-get -y update
 apt-get -y install libjpeg-dev libpng12-dev libtiff4-dev curl apache2 wget imagemagick libimage-exiftool-perl unzip lame autoconf build-essential checkinstall git libass-dev libfaac-dev libgpac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev librtmp-dev libtheora-dev libtool libvorbis-dev pkg-config texi2html zlib1g-dev ffmpeg2theora poppler-utils
