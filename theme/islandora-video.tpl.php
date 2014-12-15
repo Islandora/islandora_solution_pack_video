1f7c9e91ef3a96090fcf0a714653503a2e093aa3
Nelson Hart <nelson.hart@gmail.com>
2014-12-15 14:27:32 -0400

    solr search return url

--- theme/islandora-video.tpl.php
+++ theme/islandora-video.tpl.php
@@ -20,6 +20,7 @@
 
 <div class="islandora-video-object islandora" vocab="http://schema.org/" prefix="dcterms: http://purl.org/dc/terms/" typeof="VideoObject">
   <div class="islandora-video-content-wrapper clearfix">
+    <?php print theme('islandora_solr_search_return_link'); ?>
     <?php if ($islandora_content): ?>
       <div class="islandora-video-content">
         <?php print $islandora_content; ?>
